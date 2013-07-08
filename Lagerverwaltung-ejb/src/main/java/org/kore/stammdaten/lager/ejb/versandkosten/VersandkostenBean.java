a3f804174327733767e017ca5ca1cdb61d72da55
Konrad Renner <koni.renner@gmail.com>
2013-07-08 10:34:35 +0200

    kleinere adaptierungen

---
+++ Lagerverwaltung-ejb/src/main/java/org/kore/stammdaten/lager/ejb/versandkosten/VersandkostenBean.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.kore.stammdaten.lager.ejb.versandkosten;
+
+/**
+ *
+ * @author koni
+ */
+public class VersandkostenBean {
+    
+}
