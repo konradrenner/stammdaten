837ce585f68c7a6b278103f03af5389f70402d2e
Konrad Renner <koni.renner@gmail.com>
2014-04-06 11:47:38 +0200

    dependencies so angepasst das jetzt alles funkt

--- Lagerverwaltung/Lagerverwaltung-ejb/src/main/java/org/kore/stammdaten/lager/application/versandkosten/VersandkostenBean.java
+++ Lagerverwaltung/Lagerverwaltung-ejb/src/main/java/org/kore/stammdaten/lager/application/versandkosten/VersandkostenBean.java
@@ -18,18 +18,14 @@
  */
 package org.kore.stammdaten.lager.application.versandkosten;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Currency;
 import java.util.List;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 import javax.inject.Named;
-import org.kore.runtime.currency.Money;
-import org.kore.stammdaten.core.adresse.Land;
 import org.kore.stammdaten.lager.domain.versandkosten.DomainVersandkosten;
 import org.kore.stammdaten.lager.domain.versandkosten.Versandkosten;
 import org.kore.stammdaten.lager.domain.versandkosten.VersandkostenFactory;
@@ -73,8 +69,4 @@ public class VersandkostenBean {
         return response;
     }
 
-    public void create() {
-        Versandkosten create = factory.create(new Land("AT"), new Money(BigDecimal.ZERO, Currency.getInstance("EUR")));
-        repository.save(create);
-    }
 }
