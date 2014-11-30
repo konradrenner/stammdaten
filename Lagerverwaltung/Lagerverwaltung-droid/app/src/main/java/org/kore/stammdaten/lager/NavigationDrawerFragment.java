ec8d84fe75a0cb75a0b2954ccccb1e714629920b
Konrad Renner <koni.renner@gmail.com>
2014-11-30 21:31:15 +0100

    maven refactorings

--- Lagerverwaltung/Lagerverwaltung-droid/app/src/main/java/org/kore/stammdaten/lager/NavigationDrawerFragment.java
+++ Lagerverwaltung/Lagerverwaltung-droid/app/src/main/java/org/kore/stammdaten/lager/NavigationDrawerFragment.java
@@ -4,7 +4,7 @@ package org.kore.stammdaten.lager;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
-import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.content.SharedPreferences;
