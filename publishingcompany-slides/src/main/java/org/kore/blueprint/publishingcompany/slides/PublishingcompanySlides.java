213d17d34d748de689e4b12fd6ba368dc2914509
Konrad Renner <koni.renner@gmail.com>
2022-07-04 22:45:36 +0200

    initial commit

---
+++ publishingcompany-slides/src/main/java/org/kore/blueprint/publishingcompany/slides/PublishingcompanySlides.java
@@ -0,0 +1,15 @@
+
+
+package org.kore.blueprint.publishingcompany.slides;
+
+/**
+ *
+ * @author koni
+ */
+public class PublishingcompanySlides {
+
+    public static void main(String[] args) {
+        // maybe s
+        System.out.println("Hello World!");
+    }
+}
