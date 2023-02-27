a117ae225c0613bb01ace3558eb3f5e8e3c68867
Konrad Renner <koni.renner@gmail.com>
2023-02-27 22:06:01 +0100

    some rust coce

--- currency-service/src/main.rs
+++ currency-service/src/main.rs
@@ -1,11 +1,35 @@
+use serde::{Serialize};
+
 #[macro_use] extern crate rocket;
 
-#[get("/rust")]
-fn greeting() -> &'static str {
-    "Hello from Rust"
+#[derive(Serialize)]
+struct ConversionRate {
+  fromCurrency: String,
+  toCurrency: String,
+  conversionRate: f64, 
+}
+
+#[get("/conversion-rates?fromCurrency=<fromCurrency>&toCurrency=<toCurrency>")]
+fn conversionRates(fromCurrency: &str, toCurrency: &str) -> String {
+    let rateValue = 0.8;
+
+    let loweredFrom = fromCurrency.to_uppercase();
+    let loweredTo = toCurrency.to_uppercase();
+
+    if loweredFrom.eq(&loweredTo) {
+        rateValue = 1.0;
+    }
+
+    let cRate = ConversionRate {
+        fromCurrency: loweredFrom.to_string(),
+        toCurrency: loweredTo.to_string(),
+        conversionRate: rateValue,
+    };
+
+    serde_json::to_string(&cRate).unwrap()
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![greeting])
+    rocket::build().mount("/", routes![conversionRates])
 }
